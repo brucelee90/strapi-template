51a09ceefe87397180e5648569e4ca609206df35
Lee Klopfers <lee.klopfers@karcher.com>
2025-08-12 08:38:19 +0200

    edits server.js

--- backend/config/env/production/server.js
+++ backend/config/env/production/server.js
@@ -3,4 +3,9 @@ module.exports = ({ env }) => ({
   dirs: {
     public: "/data/public"
   },
+    app: {
+    // Hier ist die wichtige Zeile:
+    // Sie liest die Komma-getrennten Werte aus der APP_KEYS Variable.
+    keys: env.array('APP_KEYS'),
+  },
 });
