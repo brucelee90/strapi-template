98df979f7bf610fc7c8fe233fc504c5169e7f8ed
Lee Klopfers <lee.klopfers@karcher.com>
2025-08-14 08:51:51 +0200

    adds components as packages

--- backend/types/generated/components.d.ts
+++ backend/types/generated/components.d.ts
@@ -1,3 +1,24 @@
-/*
- * The app doesn't have any components yet.
- */
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface HeroNewComponent extends Struct.ComponentSchema {
+  collectionName: 'components_hero_new_components';
+  info: {
+    displayName: 'New Component';
+    icon: 'alien';
+  };
+  attributes: {
+    testMedia: Schema.Attribute.Media<
+      'images' | 'files' | 'videos' | 'audios',
+      true
+    >;
+    testText: Schema.Attribute.String;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'hero.new-component': HeroNewComponent;
+    }
+  }
+}
